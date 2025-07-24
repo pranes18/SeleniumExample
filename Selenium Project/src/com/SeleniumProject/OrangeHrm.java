5ef897135735ad945ad80ebfe03afb9bed83c6e0
pranes18 <121960316+pranes18@users.noreply.github.com>
2025-07-24 16:26:13 +0530

    Add files via upload

---
+++ Selenium Project/src/com/SeleniumProject/OrangeHrm.java
@@ -0,0 +1,29 @@
+package com.SeleniumProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OrangeHrm {
+
+	public static void main(String[] args) {
+		WebDriver driver= new ChromeDriver();
+		WebDriver driver2= new ChromeDriver();
+
+		WebDriver driver3= new ChromeDriver();
+
+		WebDriver driver4= new ChromeDriver();
+		WebDriver driver5= new ChromeDriver();
+
+		
+		driver.get("https://opensource-demo.orangehrmlive.com");
+		driver2.get("https://demo.opencart.com");
+		driver3.get("https://www.bing.com");
+		driver4.get("https://www.freecrm.com");
+		driver5.get("https://www.half.ebay.com");
+		
+		
+		
+
+}
+
+}
